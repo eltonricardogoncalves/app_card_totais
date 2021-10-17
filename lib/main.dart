0c06bbd5de554134e206655564a43a0e383f9ece
eltonricardogoncalves <eltonricardogoncalves@hotmail.com>
2021-10-17 19:28:59 -0300

    cards e tabelas

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Card e tabelas',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
